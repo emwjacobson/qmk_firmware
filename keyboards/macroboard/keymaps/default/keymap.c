14cdca677abebae97b2596591b11fdd1faa9b3d8
Emerson Jacobson <emwjacobson@gmail.com>
2022-03-21 05:30:38 +0000

    Removed key that does not exist

--- keyboards/macroboard/keymaps/default/keymap.c
+++ keyboards/macroboard/keymaps/default/keymap.c
@@ -1,9 +1,23 @@
 #include QMK_KEYBOARD_H
 
+/*
+* ┌───┬───┬───┐
+* │K00│   │K02│
+* ├───┼───┼───┤
+* │K10│K11│K12│
+* ├───┼───┼───┤
+* │K20│K21│K22│
+* └───────┴───┘
+*/
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_1,   KC_NO,   KC_2,
+        TO(0),  TO(1),
         KC_Q,   KC_W,   KC_E,
         KC_A,   KC_S,   KC_D
+    ),
+    [1] = LAYOUT(
+        TO(0),  TO(1),
+        KC_1,   KC_2,   KC_3,
+        KC_4,   KC_5,   KC_6
     )
 };
